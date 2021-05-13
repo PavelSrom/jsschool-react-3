baeecbffb4d9e8748c092f81ac2203a30a57ba56
Pavel Srom <srompavel98@gmail.com>
2021-05-13 13:34:02 +0200

    fix(homepage): add interactivity

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
+
 export const Home: React.FC = () => {
+  const history = useHistory()
+
   return (
-    <div>
-      <p className="text-red-500">Home page</p>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Button variant="contained" color="primary" onClick={() => history.push('/books')}>
+        Go to books
+      </Button>
     </div>
   )
 }
